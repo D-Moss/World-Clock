1c6e31689de1c964a2a27c3128095f360959f6f8
Diona Moss <123892824+D-Moss@users.noreply.github.com>
2024-08-23 18:08:38 -0400

    Add files via upload
    made changes to cities in html
    update multi city time zones in js

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
 function updateTime() {
+	//Philadelphia
+	let philadelphiaElement = document.querySelector("#phila");
+	if (philadelphiaElement) {
+		let philadelphiaDateElement = philadelphiaElement.querySelector(".date");
+		let philadelphiaTimeElement = philadelphiaElement.querySelector(".time");
+		let philadelphiaTime = moment().tz("America/New_York");
 
-//Philadelphia
-let philadelphiaElement = document.querySelector("#phila");
-let philadelphiaDateElement = philadelphiaElement.querySelector(".date");
-let philadelphiaTimeElement = philadelphiaElement.querySelector(".time");
-let philadelphiaTime = moment().tz("America/New_York");
+		philadelphiaDateElement.innerHTML = philadelphiaTime.format("dddd, MMMM D, YYYY");
+		philadelphiaTimeElement.innerHTML = philadelphiaTime.format("h:mm:ss [<small>]A[</small>]");
+	}
 
-philadelphiaDateElement.innerHTML = philadelphiaTime.format("dddd, MMMM D, YYYY");
-philadelphiaTimeElement.innerHTML = philadelphiaTime.format("h:mm:ss [<small>]A[</small>]");
+	//Hawaii
+	let hawaiiElement = document.querySelector("#hawaii");
+	if (hawaiiElement) {
+		let hawaiiDateElement = hawaiiElement.querySelector(".date");
+		let hawaiiTimeElement = hawaiiElement.querySelector(".time");
+		let hawaiiTime = moment().tz("Europe/Paris");
 
-//Hawaii
-let hawaiiElement = document.querySelector("#hawaii");
-let hawaiiDateElement = hawaiiElement.querySelector(".date");
-let hawaiiTimeElement = hawaiiElement.querySelector(".time");
-let hawaiiTime = moment().tz("US/Hawaii");
+		hawaiiDateElement.innerHTML = hawaiiTime.format("dddd, MMMM D, YYYY");
+		hawaiiTimeElement.innerHTML = hawaiiTime.format("h:mm:ss [<small>]A[</small>]");
+	}
+}
 
-hawaiiDateElement.innerHTML = hawaiiTime.format("dddd, MMMM D, YYYY");
-hawaiiTimeElement.innerHTML = hawaiiTime.format("h:mm:ss [<small>]A[</small>]");
+function updateCity(event) {
+	let cityTimeZone = event.target.value;
+	let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+	let cityTime = moment().tz(cityTimeZone);
+	let citiesElement = document.querySelector("#cities");
+	citiesElement.innerHTML = `
+	<div class="city">
+		<div>
+			<h2>${cityName}</h2>
+			<div class="date">${cityTime.format("dddd, MMMM D, YYYY")}</div>
+		</div>
+		<div class="time">
+			${cityTime.format("h:mm:ss")} 
+			<small>${cityTime.format("A")}</small>
+		</div>
+	</div>
+	`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
